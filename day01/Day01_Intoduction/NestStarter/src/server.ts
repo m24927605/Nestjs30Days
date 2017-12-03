da1b93c731d21bb3e5e9bf2c3d740a0bd5ed9eb4
Michael <m24927605@gmail.com>
2017-12-03 14:26:23 +0800

    參數改成引數

--- day01/Day01_Intoduction/NestStarter/src/server.ts
+++ day01/Day01_Intoduction/NestStarter/src/server.ts
@@ -5,13 +5,13 @@ import * as express from 'express';//使用express模組
 
 //創建express 實例
 const instance = express();
-//NestFactory.create()接受一個模組參數，和一個可選的express實例參數，並返回Promise。
+//NestFactory.create()接受一個模組引數，和一個可選的express實例引數，並返回Promise。
 const app: Promise<INestApplication> = NestFactory.create(ApplicationModule, instance);
 
 app
-  //Promise傳入nest的實例參數。
+  //Promise傳入nest的實例引數。
   .then(nestInstance =>
-    //nest實例具有listen方法，傳入port參數，和一個可選的callback function。
+    //nest實例具有listen方法，傳入port引數，和一個可選的callback function。
     nestInstance.listen(3000, () => {
       console.log('Application based on Express is listening on port 3000');
     })
