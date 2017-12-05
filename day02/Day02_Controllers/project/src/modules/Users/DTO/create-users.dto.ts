64003261afc70b3d4b42b901e7631aac591a5193
Michael <m24927605@gmail.com>
2017-12-05 23:18:13 +0800

    刪除乾淨

--- day02/Day02_Controllers/project/src/modules/Users/DTO/create-users.dto.ts
+++
@@ -1,4 +0,0 @@
-export class CreateUserDTO{
-    readonly _name:string;
-    readonly _age:number;
-}
--- day02/Day02_Controllers/project/src/modules/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UsersController } from './Users/users.controller';
-
-@Module({
-  modules: [],
-  controllers: [UsersController],
-  components: [],
-})
-export class ApplicationModule { }
