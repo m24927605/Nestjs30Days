b17d5ba64223a2eb53534659028f9a9bc3059b23
King <davidtofunmidada@gmail.com>
2023-01-29 17:33:17 +0100

    fix the column name
    Rename the column name from "nam e" to "name"

--- day16/Day16_SQL(Sequelize)/project/src/app/Users/users.entity.ts
+++ day16/Day16_SQL(Sequelize)/project/src/app/Users/users.entity.ts
@@ -11,7 +11,7 @@ export class Users extends Model<Users>{
     @Column({ type: DataType.INTEGER, autoIncrement: true, primaryKey: true, unique: true, field: 'ID' })
     public ID: number;
 
-    @Column({ type: DataType.STRING(50), allowNull: true, comment: '姓名', field: 'Nam e'})
+    @Column({ type: DataType.STRING(50), allowNull: true, comment: '姓名', field: 'Name'})
     public Name: string;
 
     @Column({ type: DataType.INTEGER, allowNull: true, comment: '年紀',field:'Age' })
